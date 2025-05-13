3777b2ee38ec11293fe6da94cb356f2664620b30
Peter Kester <peter.kester@solace.com>
2025-05-13 17:25:48 +0200

    Updated the application to handle multiple applicationDomains
    handles deletion of manually added sub objects in acls and queues

--- deployer/config_push.py
+++ deployer/config_push.py
@@ -16,19 +16,21 @@ def config_push(parameters):
     ep = parameters["eventPortal"]
     if not isinstance(ep, EventPortal):
         raise TypeError("Expect an EventPortal instance")
-    # Gets the application to perform the action on, defaults to all applications in the json config file
-    applications = [app for app in target["applications"] if app["name"] in apps] if apps else  target["applications"]
-    for application in applications:
-        app_name = application["name"]
-        if application.get("versionId"):
-            version_id = application["versionId"]
-            for broker_id in target["broker_ids"]:
-                logging.info(f"{ action.capitalize() } for application { app_name } with version { application["version"] } and id { version_id } on broker {broker_id}")
-                try:
-                    preview_broker_id = preview.get('broker_id') if action == "undeploy" else broker_id
-                    application_preview = ep.preview_application_deployment(version_id, "deploy", preview_broker_id)
-                    ep.create_application_deployment(version_id, action, broker_id )
-                except EventPortalException as ex:
-                    logging.error(f"config_push::Exception::{ex}")
-        else:
-            logging.info(f"Application { app_name } with version { application["version"] } is not eligible for action { action }")
+    # Gets the application to perform the action on, defaults to all applications of all domains in the json config file
+    for domain in target.get("domains"):
+        domain_name = domain.get("domainName")
+        applications = domain.get("applications")
+        for application in [app for app in applications if app["versionId"]]:
+            app_name = application.get("name")
+            if application.get("versionId"):
+                version_id = application["versionId"]
+                for broker_id in target["broker_ids"]:
+                    logging.info(f"{ action.capitalize() } for application { app_name } with version { application["version"] } and id { version_id } on broker {broker_id} for application domain {domain.get("domainName")}")
+                    try:
+                        preview_broker_id = preview.get('broker_id') if action == "undeploy" else broker_id
+                        application_preview = ep.preview_application_deployment(version_id, "deploy", preview_broker_id)
+                        ep.create_application_deployment(version_id, action, broker_id )
+                    except EventPortalException as ex:
+                        logging.error(f"config_push::Exception::{ex}")
+            else:
+                logging.info(f"Application { app_name } with version { application["version"] } in domain {domain.get("domainName")} is not eligible for action { action }")
